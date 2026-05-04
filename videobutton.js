3448cd45c32dfb705533bab3e4b14527c0f01c83
TobiasM32 <tobias8@hawaii.edu>
2026-05-04 03:08:46 -0700

    Update videobutton.js

--- videobutton.js
+++ videobutton.js
@@ -1,1 +1,20 @@
+/*
+Student Name: Tobias McMillan
+File Name: HomeVidButton.js
+Date: 12/14/2025
+*/
 
+// https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+// makes the button show/hide the table of videos
+const button = document.getElementById("toggleVideos");
+const section = document.getElementById("kindermusikVid");
+
+button.addEventListener("click", () => {
+    if (window.getComputedStyle(section).display === "none") {
+        section.style.display = "block";
+        button.textContent = "Show/hide my favorite songs";
+    } else {
+        section.style.display = "none";
+        button.textContent = "Show/hide my favorite songs";
+    }
+});
